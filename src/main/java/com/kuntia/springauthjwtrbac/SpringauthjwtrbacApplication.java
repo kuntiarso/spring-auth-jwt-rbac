bf22cf46a6cbb9d39c919129d9314272c7875115
kuntiarso <gilang.grk17@gmail.com>
2023-06-14 20:06:08 +0700

    chore: adjust better env management

--- src/main/java/com/kuntia/springauthjwtrbac/SpringauthjwtrbacApplication.java
+++ src/main/java/com/kuntia/springauthjwtrbac/SpringauthjwtrbacApplication.java
@@ -2,8 +2,9 @@ package com.kuntia.springauthjwtrbac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringauthjwtrbacApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kuntia/springauthjwtrbac/user/UserController.java
+++ src/main/java/com/kuntia/springauthjwtrbac/user/UserController.java
@@ -1,5 +1,9 @@
 package com.kuntia.springauthjwtrbac.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +11,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-
 @RestController
-@AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
     private UserService userService;
+
+    @Value("${spring.datasource.url}")
+    private String URL;
+
+    @Value("${spring.datasource.username}")
+    private String USERNAME;
+
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+
+    @GetMapping("/url")
+    public ResponseEntity<?> getUrl() {
+        Map<String, String> env = new HashMap<String, String>();
+        env.put("url", URL);
+        env.put("username", USERNAME);
+        env.put("password", PASSWORD);
+
+        return new ResponseEntity<>(env, HttpStatus.OK);
+    }
 
     @GetMapping
     public ResponseEntity<User> getUser(@RequestParam String email) {
